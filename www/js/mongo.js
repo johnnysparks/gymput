efcd40ed776fda52d2a746546e2b12041f9a4442
Joe Nelson and Johnny Fuchs <dev+begriffs+johnnyfuchs@bendyworks.com>
2012-05-19 17:36:03 -0500

    Adds Mongo#insert plus a test

--- www/js/mongo.js
+++ www/js/mongo.js
@@ -1,11 +1,22 @@
 var Mongo = Koi.define({
   init: function(options) {
-    options   = options || {};
-    this.id   = options.api_id;
-    this.db   = options.db;
-    this.coll = options.collection;
+    options      = options || {};
+    this.api_key = options.api_key;
+    this.db      = options.db;
+    this.coll    = options.collection;
   },
-  insert: function(data) {},
+  insert: function(data, callback) {
+    $.ajax({
+      dataType: "json",
+      type: "post",
+      data: {"document" : data, 'safe': true },
+      url: "https://api.mongohq.com/databases/" +
+        this.db + "/collections/" +
+        this.coll + "/documents?_apikey=" + this.api_key,
+      success: callback,
+      error: function(response){ console.log( response ); }
+    });
+  },
   update: function(obj_id, json) {},
   get:    function(obj_id) {},
   find:   function(query, fields) {}
--- www/js/spec/mongo.js
+++ www/js/spec/mongo.js
@@ -1,7 +1,22 @@
-module("Mongo");
+describe('Mongo', function() {
+  it('saves a document to the collection', function() {
+    var entry = {
+      name: "joe",
+      phone: "12341234",
+      email: "jklasdfoih@afshudoh",
+      address: "hjl1234hljjhl h asfkjh123"
+    }
+    var response = false;
 
-test("", function() {
-  expect(2);
-  equal( true, false, "failing test" );
-  equal( true, true, "passing test" );
+    var callback = function( o ){ response = o; }
+    var mongo = new Mongo({ api_key: "dmfbybnos7h10x2rrrrx", db: "gymput", collection: "prospects" });
+    mongo.insert(entry, callback);
+    waitsFor(function(){
+      return response;
+    }, "Response never arrived", 3000);
+
+    runs( function(){
+      expect(response.ok).toEqual(1)
+    });
+  });
 });
