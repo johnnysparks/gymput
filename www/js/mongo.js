50e2965dd19b3c5f2d968c800a904cbbe7f021fa
Johnny Fuchs <johnnyfuchs@gmail.com>
2012-05-20 16:26:11 -0500

    added new tests, cleaned up mongojs class init

--- www/js/mongo.js
+++ www/js/mongo.js
@@ -1,23 +1,72 @@
+/**
+* Mongo Class
+*  Provides and interface to mongo hq.
+*/
+
 var Mongo = Koi.define({
+/**
+* init - sets up 
+*  @param {object}    options:    option object containing all available settings
+*    @param {String}  api_key:    the connection token for mongohq
+*    @param {String}  db:         database name
+*    @param {String}  collection: collection name
+*/
   init: function(options) {
-    options      = options || {};
-    this.api_key = options.api_key;
-    this.db      = options.db;
-    this.coll    = options.collection;
+    options       = options || {};
+    this.api_key  = options.api_key;
+    this.db       = options.db;
+    this.coll     = options.collection;
+    this.url      = "https://api.mongohq.com/databases/" +
+                    this.db + "/collections/" +
+                    this.coll + "/documents/";
+    this.url_api  = "?_apikey=" + this.api_key
+
+    $.ajaxSetup({ dataType: "json", error: console.log });
   },
+
+/**
+* insert - adds a document to the collections
+*  @param {object}    data: the entry to be inserted to the database
+*  @param {function}  callback: success handler function
+*/
   insert: function(data, callback) {
     $.ajax({
-      dataType: "json",
       type: "post",
       data: {"document" : data, 'safe': true },
-      url: "https://api.mongohq.com/databases/" +
-        this.db + "/collections/" +
-        this.coll + "/documents?_apikey=" + this.api_key,
-      success: callback,
-      error: function(response){ console.log( response ); }
+      url: this.url + this.url_api,
+      success: callback
     });
   },
-  update: function(obj_id, json) {},
-  get:    function(obj_id) {},
-  find:   function(query, fields) {}
+
+/**
+* update - retrieves one document from the collection based on an id
+*  @param {string}    obj_id: string representation of mongo_id
+*  @param {function}  callback: success handler function
+*/
+  update: function(obj_id, updated, callback) {
+    console.log( updated );
+    $.ajax({
+      type: "put",
+      data: {"document" : { $set: updated }, 'safe': true },
+      url: this.url + obj_id + this.url_api,
+      success: callback
+    });
+
+
+  },
+
+/**
+* get - retrieves one document from the collection based on an id
+*  @param {string}    obj_id: string representation of mongo_id
+*  @param {function}  callback: success handler function
+*/
+  get: function(obj_id, callback) {
+    console.log( this.url + obj_id + this.url_api );
+    $.ajax({
+      type: "get",
+      url: this.url + obj_id + this.url_api,
+      success: callback
+    });
+},
+  find: function(query, fields) {}
 });
--- www/js/spec/mongo.js
+++ www/js/spec/mongo.js
@@ -1,22 +1,42 @@
 describe('Mongo', function() {
+  var response = false;
+  var entry    = { name: "joe", phone: "1234", email: "jklh@afdoh", address: "hjl1234" };
+  var mongo    = new Mongo({ api_key: "dmfbybnos7h10x2rrrrx", db: "gymput", collection: "prospects" });
+  var callback = function( o ){ response = o; }
+
+  beforeEach(function(){
+    response = false;
+  });
+
   it('saves a document to the collection', function() {
-    var entry = {
-      name: "joe",
-      phone: "12341234",
-      email: "jklasdfoih@afshudoh",
-      address: "hjl1234hljjhl h asfkjh123"
-    }
-    var response = false;
 
-    var callback = function( o ){ response = o; }
-    var mongo = new Mongo({ api_key: "dmfbybnos7h10x2rrrrx", db: "gymput", collection: "prospects" });
     mongo.insert(entry, callback);
-    waitsFor(function(){
-      return response;
-    }, "Response never arrived", 3000);
 
+    waitsFor(function(){ return response; }, "Response never arrived", 3000);
     runs( function(){
       expect(response.ok).toEqual(1)
+    });
+
+  });
+
+  it('retrives a document from a collection by it\'s mongo id', function() {
+
+    mongo.get("4fb074c5fb3ac70001000028", callback);
+
+    waitsFor(function(){ return response; }, "Response never arrived", 3000);
+    runs( function(){
+      expect(response._id.$oid).toEqual("4fb074c5fb3ac70001000028");
+    });
+  });
+
+  it('updates a document with a merged in object by it\'s mongo id', function() {
+    var update_object = { name: "johnny" };
+    mongo.update("4fb074c5fb3ac70001000028", update_object, callback);
+
+    waitsFor(function(){ return response; }, "Response never arrived", 3000);
+
+    runs( function(){
+      expect(response.name).toEqual("johnny");
     });
   });
 });
