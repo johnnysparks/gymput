f8a55ce7e8053973d255ed26aeff1b52959c03da
Johnny Fuchs <johnnyfuchs@gmail.com>
2012-06-11 19:58:34 -0500

    updated app identifier and auth key for cloudmine

--- www/js/cloud.js
+++ www/js/cloud.js
@@ -1,15 +1,15 @@
 Cloud = Koi.define({
   base_path : "https://api.cloudmine.me/v1/app",
-  app_key   : "aeac05276e57434f983c366212e43ca1",
-  auth_key  : "7e2a266cdf5449aa81058ff41d9ad891",
+  app_key   : "3f9ee746eacf43498e7355a71f0e3360",
+  auth_key  : "4ecbe42938d44afda746585b1aeb009d",
 
   init: function(){
-    var _this = this;
+    var self = this;
     this.app_path = this.base_path +'/'+ this.app_key,
     $.ajaxSetup({
         dataType: "json",
         beforeSend: function(xhr) {
-            xhr.setRequestHeader("X-CloudMine-ApiKey", _this.auth_key);
+            xhr.setRequestHeader("X-CloudMine-ApiKey", self.auth_key);
             xhr.setRequestHeader("Content-Type", "application/json");
         }
     });
